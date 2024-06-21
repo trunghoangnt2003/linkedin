efe45e57c6f97545a8363d88970841c6142c29e2
havancuong2003 <havancuong07122003@gmail.com>
2024-06-21 14:31:21 +0700

    add: add clsx

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,3 +1,18 @@
-export const Home = () => {
-    return <div></div>;
+import clsx from "clsx";
+
+type Props = {
+    classes?: {
+        [key: string]: string;
+    };
+};
+export const Home: React.FC<Props> = ({ classes }) => {
+    return (
+        <>
+            <div className="flex justify-center items-center min-h-screen bg-red-500">
+                <div className="flex-1 bg-gray-300 h-full"></div>
+                <div className={clsx(classes?.mainContent, "")}>a</div>
+                <div className="flex-1 bg-gray-300 h-full"></div>
+            </div>
+        </>
+    );
 };
